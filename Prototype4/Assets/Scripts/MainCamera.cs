d0aa1b8f4d7478d9959d829863343fd8d4cd24e0
lynn901230 <yyays20130220@gmail.com>
2017-01-23 14:17:18 +0900

    コメント削除

--- Prototype4/Assets/Scripts/MainCamera.cs
+++ Prototype4/Assets/Scripts/MainCamera.cs
@@ -14,66 +14,32 @@ public class MainCamera : MonoBehaviour {
     public GameObject object2;
     private int j = 0, k = 0;
     int speed = 1;
-    public GameObject chair;
     public GameObject desk;
     public void Start () {
         buttonClick = GameObject.Find("StartButton").GetComponent<ButtonClick>();
-        chair = Instantiate(Resources.Load("Prefabs/Chair", typeof(GameObject))) as GameObject;
-        //desk = Instantiate(Resources.Load("Prefabs/Desk", typeof(GameObject))) as GameObject;
         _namelist = buttonClick.namelist;
-        //transform.position = new Vector3(5, 10, -10);
-        //transform.Rotate(new Vector3(50, 0.5f, 0));
-        //int xPos = -6;
-        //int i = 0;
-        //// Z位置を+1しながらマップを生成していく
-        //for (; xPos < 7; xPos++)
-        //{
-        //    int zPos = -4;
-        //    for (; zPos < 5; zPos++)
-        //    {
-        //        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //        myList.Add(cube);
-        //        myList[i].transform.position = new Vector3(xPos, 0, zPos);
-        //        if (i % 2 != 1)
-        //        {
-        //            myList[i].GetComponent<Renderer>().material.color = Color.black;
-        //        }
-        //        i++;
-        //    }
-        //}
+
         GameObject hero = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        hero.transform.position = new Vector3(0, 1.5f, -4);
+        hero.transform.position = new Vector3(-5, 1, 0);
         hero.GetComponent<Renderer>().material.color = Color.red;
 
-        enemy1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        enemy1.name = "enemy1";
-        enemy1.GetComponent<Renderer>().material.color = Color.blue;
-        enemy1.transform.position = new Vector3(-4, 1.5f, 4);
-        enemy1.AddComponent<Rigidbody>();
+        //主人公カメラ視点設定
+        transform.position = new Vector3(-5, 1.7f, 0);
+        transform.rotation = Quaternion.Euler(0, 90, 0);
 
-        enemy2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        enemy2 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
         enemy2.name = "enemy2";
         enemy2.GetComponent<Renderer>().material.color = Color.blue;
-        enemy2.transform.position = new Vector3(4, 1.5f, 4);
+        enemy2.transform.position = new Vector3(3, 1, 0);
+        enemy2.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
         enemy2.AddComponent<Rigidbody>();
+        enemy2.GetComponent<Rigidbody>().mass = 100;
 
-        chair.transform.position = new Vector3(-4, 1.5f, -4);
-        chair.name = "object1";
-        chair.AddComponent<Rigidbody>();
-        chair.AddComponent<BoxCollider>();
-
-        //desk.transform.position = new Vector3(4, 1, -4);
-        //desk.name = "object2";
-        //desk.AddComponent<Rigidbody>();
-        //desk.AddComponent<BoxCollider>();
-        desk = GameObject.Find("desk01");
-        //desk = GameObject.Find("desk02");
+        desk = GameObject.Find("desk02-test");
+        desk.name = "object1";
         desk.AddComponent<Rigidbody>();
         desk.AddComponent<BoxCollider>();
 
-        //Rigidbody test;
-        //test = GameObject.Find("object2").GetComponent<Rigidbody>();
-        //test.mass = 100;
         targetObject = desk;
     }
     //出力処理
@@ -89,15 +55,15 @@ public class MainCamera : MonoBehaviour {
         yield return new WaitForSeconds(speed / 10f * j + k / 2f);//           Debug.Log(speed / 5f * (j + 1) * (k + 1));
         if (number == "50")
         {
-            targetObject.transform.position += new Vector3(0, 0, 1);
+            targetObject.transform.position += new Vector3(1, 0, 0);
         }//50の処理をするunity側のコードを記述 straight
         else if (number == "51")
         {
-            targetObject.transform.position += new Vector3(-1, 0, 0);
+            targetObject.transform.position += new Vector3(0, 0, 1);
         }//51の処理をするunity側のコードを記述 left
         else if (number == "52")
         {
-            targetObject.transform.position += new Vector3(1, 0, 0);
+            targetObject.transform.position += new Vector3(0, 0, -1);
         }//52の処理をするunity側のコードを記述 right
     }
 
--- Prototype4/Assets/Scripts/Reset.cs
+++ Prototype4/Assets/Scripts/Reset.cs
@@ -12,45 +12,24 @@ public class Reset : MonoBehaviour
     public void OnClick()
     {
         maincamera = GameObject.Find("Main Camera").GetComponent<MainCamera>();
+        GameObject destroytarget;
 
-        GameObject destroytarget = GameObject.Find("object1");
+        destroytarget = GameObject.Find("object1");
         Destroy(destroytarget);
-        GameObject chair = Instantiate(Resources.Load("Prefabs/Chair", typeof(GameObject))) as GameObject;
-        chair.transform.position = new Vector3(-4, 2, -4);
-        chair.name = "object1";
-        chair.AddComponent<Rigidbody>();
-        chair.AddComponent<BoxCollider>();
-
-        destroytarget = GameObject.Find("object2");
-        Destroy(destroytarget);
-        GameObject desk = Instantiate(Resources.Load("Prefabs/Desk", typeof(GameObject))) as GameObject;
-        desk.transform.position = new Vector3(4, 2, -4);
-        desk.name = "object2";
+        GameObject desk = Instantiate(Resources.Load("Prefabs/desk02-test", typeof(GameObject))) as GameObject;
+        desk.transform.position = new Vector3(1, 0, 0);
+        desk.name = "object1";
         desk.AddComponent<Rigidbody>();
         desk.AddComponent<BoxCollider>();
-        //destroytarget = GameObject.Find("desk01");
-        //Destroy(destroytarget);
-        //GameObject desk = GameObject.Find("desk01");
         maincamera.targetObject = desk;
 
-        destroytarget = GameObject.Find("enemy1");
-        Destroy(destroytarget);
-        GameObject enemy1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        enemy1.name = "enemy1";
-        enemy1.GetComponent<Renderer>().material.color = Color.blue;
-        enemy1.transform.position = new Vector3(-4, 2, 4);
-        enemy1.AddComponent<Rigidbody>();
-        maincamera.enemy1= enemy1;
-
-        destroytarget = GameObject.Find("enemy2");
-        Destroy(destroytarget);
-        GameObject enemy2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        GameObject enemy2 = GameObject.Find("enemy2");
         enemy2.name = "enemy2";
         enemy2.GetComponent<Renderer>().material.color = Color.blue;
-        enemy2.transform.position = new Vector3(4, 2, 4);
-        enemy2.AddComponent<Rigidbody>();
+        enemy2.transform.position = new Vector3(3, 1, 0);
+        enemy2.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        enemy2.GetComponent<Rigidbody>().mass = 100;
         maincamera.enemy2 = enemy2;
 
-        Destroy(destroytarget);
     }
 }
