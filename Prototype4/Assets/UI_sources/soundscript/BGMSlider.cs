65e72c708dde2df1e40c93212540aad8022a3e7e
ArinanirA <rina.asai@cpc.kyushu-u.ac.jp>
2017-01-30 14:06:41 +0900

    音量調節機能実装

---
+++ Prototype4/Assets/UI_sources/soundscript/BGMSlider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGMSlider : MonoBehaviour {
+    private GameObject BGMslider;
+    private GameObject SM;
+    // Use this for initialization
+    void Start () {
+        this.BGMslider = GameObject.Find("BGMSlider");
+        this.SM = GameObject.Find("SoundManager");
+        float BGM = this.SM.GetComponent<SoundManager>().volume.BGM;
+        BGMslider.GetComponent<Slider>().value = BGM;
+    }
+}
