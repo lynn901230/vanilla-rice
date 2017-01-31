9fed48d171fc107fca860d7a3e78d9b4b9bda6b5
ArinanirA <rina.asai@cpc.kyushu-u.ac.jp>
2017-01-31 14:39:24 +0900

    メニュー画面テキスト変更機能

---
+++ Prototype4/Assets/UI_sources/script_UI/menuText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuText : MonoBehaviour {
+    public Text menutext;
+    string[] str = { "お疲れさま！今日は何があった？", "これから何をしよっか？", "ちょこっと休憩したいかも" };
+        // Use this for initialization
+    void Start() {
+        menutext.text = str[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            menutext.text = str[UnityEngine.Random.Range(0, 3)];
+        }
+    }
+}
